ad6ac512267c5e72b66d3063e651304634df7674
RobIsr <israelsson.r@gmail.com>
2017-12-13 22:51:48 +0100

    Re-added Results and Workouts.java. They somehow dissapeared.

---
+++ app/src/main/java/com/example/israelsson/onemorerepfitness/models/Workouts.java
@@ -0,0 +1,45 @@
+package com.example.israelsson.onemorerepfitness.models;
+
+/**
+ * Created by israe on 2017-09-15.
+ */
+
+public class Workouts {
+    private String description;
+    private String exersize_1;
+    private String exersize_2;
+    private String exersize_3;
+    private String exersize_4;
+
+    public Workouts() {
+
+    }
+
+    public Workouts(String description, String exersize_1, String exersize_2, String exersize_3, String exersize_4) {
+        this.description = description;
+        this.exersize_1 = exersize_1;
+        this.exersize_2 = exersize_2;
+        this.exersize_3 = exersize_3;
+        this.exersize_4 = exersize_4;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public String getExersize_1() {
+        return this.exersize_1;
+    }
+
+    public String getExersize_2() {
+        return this.exersize_2;
+    }
+
+    public String getExersize_3() {
+        return this.exersize_3;
+    }
+
+    public String getExersize_4() {
+        return this.exersize_4;
+    }
+}
